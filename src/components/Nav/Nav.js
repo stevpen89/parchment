dc9e2dec74ade0c0e73ce14ae5554d2e5b098d94
TarynFicklin <38847691+TarynFicklin@users.noreply.github.com>
2018-10-04 18:21:19 -0600

    added CSS to the nav, and menu, added font awesome

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 import Menu from '../Menu/Menu'
+import './Nav.css'
 
 class Nav extends Component {
 	constructor(){
 		super()
-		this.state={}
+		this.state={
+			menuOpen: false
+		}
+		this.openMenu = this.openMenu.bind(this);
 	}
+
+	//opens and closes the menu
+	openMenu () {this.setState({menuOpen: !this.state.menuOpen})}
+
 	render() {
+		const {openMenu} = this
+		const {menuOpen} = this.state
 		return (
 			<div>
-				This is Nav
-				<Menu/>
+				<div className="nav">
+					<div onClick={() => openMenu()}><i className="fas fa-bars menu-button"></i></div>
+					<div><h1>PARCHMENT</h1></div>
+					<div>
+						<a className="menu-button">Login</a>
+						<i className="fas fa-shopping-cart menu-button"></i>
+					</div>
+				</div>
+				<Menu menuOpen={menuOpen}/>
 			</div>
 		)
 	}
