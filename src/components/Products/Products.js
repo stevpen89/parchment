736c12225add3cb9dc94722eb181f9ec0aeedb4d
Steven Penfield <stevenpenfield9@gmail.com>
2018-10-05 00:33:13 -0600

    couples tree form finished in functionality. it's a monster... but it's my monster...

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import Filters from '../Filters/Filters'
+import BabyBlanket from '../BabyBlanket/BabyBlanket'
+import CrazyBlanket from '../CrazyBlanket/CrazyBlanket'
+import BinaryBlanket from '../BinaryBlanket/BinaryBlanket'
 
 class Products extends Component {
 	constructor(){
 		super()
-		this.state={}
+		this.state={
+			
+		}
 	}
 	render() {
 		return (
 			<div>
 				This is Products
 				<Filters/>
+				<BabyBlanket/>
+				<CrazyBlanket/>
+				<BinaryBlanket/>
 			</div>
 		)
 	}
