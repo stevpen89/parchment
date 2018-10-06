8aa349c9e6e1955de031c7421c5f8915241ec09b
TarynFicklin <38847691+TarynFicklin@users.noreply.github.com>
2018-10-05 21:00:20 -0600

    now uploads a text file to S3

--- server/controllers/s3Controller.js
+++ server/controllers/s3Controller.js
@@ -1,16 +1,16 @@
-const { S3_BUCKET, S3_ACCESS_KEY, S3_SECRET_KEY } = process.env;
 const AWS  = require ( 'aws-sdk' ),
-      path = require ( 'path'    ),
-			fs   = require ( 'fs'      )
-			
+      path = require ( 'path' ),
+			fs   = require ( 'fs' )
+
 module.exports = {
 	bucket: () => {
 
+		const { S3_ACCESS_KEY, S3_SECRET_KEY, S3_BUCKET } = process.env
 		AWS.config.update ( {accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY} );
 
 		let s3       = new AWS.S3();
-		let filePath = "./data/file.txt"; //change this to the file that we want upload
-		let params   = { Bucket: S3_BUCKET, Body: fs.createReadStream(filePath), Key: `folder/${Date.now()}_${path.basename(filePath)}`	};
+		let filePath = "./file.txt";
+		let params   = { Bucket: S3_BUCKET, Body: fs.createReadStream(filePath), Key: `file/${Date.now()}_${path.basename(filePath)}`	};
 
 		s3.upload(params, function (err, data) {
 			err  ? console.log ( "Error", err ) : null;
