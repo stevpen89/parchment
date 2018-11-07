183724aaf8a65453107130d377842262b844b375
Steven Penfield <stevenpenfield9@gmail.com>
2018-11-06 23:54:18 -0700

    stripe total accurate, has edge case exclusions, and is looking pretty good to go. still needs live keys.

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -4,10 +4,33 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 module.exports = {
 
     handlePayment: (req, res) => {
-        const { amount, token:{id}} = req.body
+
+        const amountArray = req.body.amount.toString().split('')
+        const pennies = []
+        for (var i = 0; i<amountArray.length; i++){
+            if(amountArray[i]==='.'){
+                if (typeof amountArray[i + 1]==='string'){
+                    pennies.push(amountArray[i + 1])
+                } else {
+                    pennies.push('0')
+                }
+                if (typeof amountArray[i + 2]==='string'){
+                    pennies.push(amountArray[i+2])
+                } else {
+                    pennies.push('0')
+                }
+                break;
+            } else {
+                pennies.push(amountArray[i])
+            }
+        }
+        const convertedAmt = parseInt(pennies.join(''))
+
+        const {token:{id}} = req.body
+        
         stripe.charges.create(
             {
-                amount: amount,
+                amount: convertedAmt,
                 currency: "usd",
                 source: id,
                 description: "Test charge from Steven"
