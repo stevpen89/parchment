3fab2e086b039be94cf572ccc33fbf726807c5ba
Steven Penfield <stevenpenfield9@gmail.com>
2018-11-07 20:58:53 -0700

    added product seed, fixed some tagging/ url issues/ should be good.

--- db/master_seed.sql
+++ db/master_seed.sql
@@ -1,6 +1,5 @@
 DROP TABLE presets;
 DROP TABLE orders;
-DROP TABLE products;
 DROP TABLE cards;
 DROP TABLE users;
 
@@ -84,42 +83,6 @@ SELECT * FROM cards;
 
 
 
-CREATE TABLE products (
-  product_sku      SERIAL PRIMARY KEY,
-  product_name     VARCHAR (200),
-  product_tags     JSON,
-  product_image    TEXT,
-  product_thumbs   JSON,
-  product_desc     VARCHAR(8000),
-  product_type     VARCHAR(100),
-  product_price    FLOAT,
-  product_sale     FLOAT,
-  product_shipping FLOAT,
-  o1               TEXT
-);
-
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping)
-VALUES ('Binary Family History Blanket', '{"tags":["blanket","binary","animals","geometric"]}', 'https://pbs.twimg.com/profile_images/878962163737665537/OLAUQNQe_400x400.jpg', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'blanket', 12.99, null, 0);
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping)
-VALUES ('Parents and Kids Family History Blanket', '{"tags":["blanket","inverted","family","geometric","geneaology"]}', 'https://images.unsplash.com/photo-1540844775339-de8c67e13da4?ixlib=rb-0.3.5&s=fc4fae50596e7b8bea134cc641acea2c&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'blanket', 12.99, null, 0);
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('Personalized Missionary Journal', '{"tags":["journal","missionary","state","alabama"]}', 'https://images.unsplash.com/photo-1540889539617-6236fa68e2f0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=64923c5e7a9e7b435b9058949a2cb5d8&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'journal', 12.99, null, 3.99, '["title","subtitle","date"]');
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping)
-VALUES ('Bear Baby Blanket', '{"tags":["blanket","baby","animals","geometric"]}', 'https://images.unsplash.com/photo-1540845692348-b9d2bc813a63?ixlib=rb-0.3.5&s=b0c0bbbebc81382380cd4b826ba5083d&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'blanket', 12.99, null, 0);
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('EveryDay Journal Mountains', '{"tags":["journal","animal","nature","scenic"]}', 'https://images.unsplash.com/photo-1540877644327-477011fe0afd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6e288ba6f8f31ef9b859f41ef9a5d9b7&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'journal', 12.99, null, 3.99, '["title","subtitle"]');
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('EveryDay Journal Mountains', '{"tags":["journal","animal","nature","scenic"]}', 'https://images.unsplash.com/photo-1540877644327-477011fe0afd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6e288ba6f8f31ef9b859f41ef9a5d9b7&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'journal', 12.99, null, 3.99, '["title","subtitle","date"]');
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('EveryDay Journal Mountains', '{"tags":["journal","animal","nature","scenic"]}', 'https://images.unsplash.com/photo-1540877644327-477011fe0afd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6e288ba6f8f31ef9b859f41ef9a5d9b7&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'journal', 12.99, null, 3.99, '["title","subtitle","date"]');
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('EveryDay Journal Mountains', '{"tags":["blanket","single","nature","scenic"]}', 'https://images.unsplash.com/photo-1540877644327-477011fe0afd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6e288ba6f8f31ef9b859f41ef9a5d9b7&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'journal', 12.99, null, 3.99, '["title","subtitle","date"]');
-INSERT INTO products (product_name, product_tags, product_image, product_thumbs, product_desc, product_type, product_price, product_sale, product_shipping, o1)
-VALUES ('Cold Outside Holiday Blanket', '{"tags":["blanket","holiday","snow","cute"]}', 'https://images.unsplash.com/photo-1418662589339-364ad47f98a2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=235f484b2cdafba4d0e2fa382f09286f&auto=format&fit=crop&w=400&q=80', '{"thumbnails":["http://image.jpg","http://image.jpg"]}', 'ultra fuzzy soft lorem ipsum blanket waifu...', 'blanket', 12.99, null, 0, '["title","subtitle","date"]');
-SELECT * FROM products;
-
-
-
 CREATE TABLE orders (
   order_id         SERIAL PRIMARY KEY,
   user_id          INTEGER,
@@ -138,12 +101,9 @@ CREATE TABLE orders (
 SELECT * FROM orders;
 
 
+
 CREATE TABLE presets (
   preset_id      SERIAL PRIMARY KEY,
   preset_name    VARCHAR (200),
   preset_data    TEXT
 );
-
-
-
-SELECT * FROM products;
