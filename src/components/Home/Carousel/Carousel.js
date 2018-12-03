6d7d6dd01a74fb6c910af89f5c5eebfa8c192249
Steven Penfield <stevenpenfield9@gmail.com>
2018-12-03 07:57:30 -0700

    removed black friday banner

--- src/components/Home/Carousel/Carousel.js
+++ src/components/Home/Carousel/Carousel.js
@@ -12,8 +12,8 @@ class Carousel extends Component {
 			<div className="carousel">
 				<div className="content-alignment">
 					<img src="https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/logo.png" alt="parchment logo" className="parchment-logo"/><br />
-					{/* <Link to="/products"><button>Order Now</button></Link> */}
-					<img src="https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/Black_Friday_Sale.png" id="announcement"/>
+					<Link to="/products"><button>Order Now</button></Link>
+					{/* <img src="https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/Black_Friday_Sale.png" id="announcement"/> */}
 					<div className="broken">Please contact us at sales@parchmentgoods.com if you experience any difficulty placing an order. Thank you.</div>
 					<div className="down-arrow"><i class="fas fa-chevron-down"></i></div>
 				</div>
