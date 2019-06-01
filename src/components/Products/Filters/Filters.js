3983aa132ea2f1361a2809b0929bd2112f5459f8
Steven Penfield <stevenpenfield9@gmail.com>
2019-06-01 12:59:07 -0600

    Code with Taryn, from 6/1/2019

--- src/components/Products/Filters/Filters.js
+++ src/components/Products/Filters/Filters.js
@@ -49,7 +49,632 @@ class Filters extends Component {
 			}
 		}
     fillTags();
-    axios.put('/products/search', {tags: percentified}).then(res => {this.props.setProducts(res.data)})
+		axios.put('/products/search', {tags: percentified}).then(res => {
+			let data = res.data
+			let stringData = JSON.stringify(data)
+			if(stringData.includes("journal_missionary")){			
+				axios.put('/products/search', {tags: ["%generic%", "%%", "%%", "%%", "%%", "%%", "%%", "%%", "%%", "%%"]}).then(res => {
+					res.data.map((x)=>{data.push(x)})})
+					// this.props.setProducts(data)
+				}
+			else{			
+				console.log("it didn't work ya poop") }
+			let poop = [
+				{
+					"product_sku": 6,
+					"product_name": "Barbados Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"leather",
+							"missionary",
+							"country",
+							"barbados",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_country_outline/Barbados.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_missionary",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 208,
+					"product_name": "Complete World Map Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"complete world map",
+							"outline",
+							"map",
+							"world",
+							"globe"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Complete_World_Map.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 209,
+					"product_name": "Footsteps Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"footsteps",
+							"feet",
+							"shoes",
+							"path",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Footsteps.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 210,
+					"product_name": "Name Tag Elder Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"name tag elder",
+							"shirt",
+							"boys",
+							"elders",
+							"tie",
+							"suit",
+							"tag",
+							"priesthood",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Name_Tag_Elder.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 211,
+					"product_name": "Name Tag Sister Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"name tag sister",
+							"blouse",
+							"skirt",
+							"sister",
+							"tag",
+							"mission",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Name_Tag_Sister.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 212,
+					"product_name": "Name Tag With Scripture Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"name tag with scripture",
+							"plain",
+							"simple",
+							"mission",
+							"elder",
+							"sister",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Name_Tag_With_Scripture.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 213,
+					"product_name": "Preach The Gospel Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"preach the gospel",
+							"preach",
+							"duty",
+							"important",
+							"work",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Preach_The_Gospel.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 214,
+					"product_name": "Salt Lake Temple Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"salt lake temple",
+							"salt",
+							"lake",
+							"city",
+							"temple",
+							"utah",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Salt_Lake_Temple.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 215,
+					"product_name": "Shepherd Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"shepherd",
+							"sheep",
+							"crook",
+							"staff",
+							"hand",
+							"flock",
+							"fold",
+							"scripture",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Shepherd.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 216,
+					"product_name": "Tree Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"tree",
+							"path",
+							"life",
+							"commence",
+							"rod",
+							"iron",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Tree.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 217,
+					"product_name": "World Map Country Outlines Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"world map country outlines",
+							"globe",
+							"international",
+							"earth",
+							"world",
+							"big",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/World_Map_Country_Outlines.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 218,
+					"product_name": "World Map Silhouette Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"world map silhouette",
+							"globe",
+							"international",
+							"earth",
+							"world",
+							"big",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/World_Map_Silhouette.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 219,
+					"product_name": "Wreath 1 Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"wreath 1",
+							"leaves",
+							"wreath",
+							"nature",
+							"christmas",
+							"symbolic",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Wreath_1.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 220,
+					"product_name": "Wreath 2 Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"wreath 2",
+							"leaves",
+							"wreath",
+							"nature",
+							"christmas",
+							"symbolic",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Wreath_2.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 221,
+					"product_name": "Wreath 3 Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"wreath 3",
+							"leaves",
+							"wreath",
+							"nature",
+							"christmas",
+							"symbolic",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Wreath_3.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				},
+				{
+					"product_sku": 222,
+					"product_name": "Wreath 4 Outline Journal",
+					"product_tags": {
+						"tags": [
+							"journal",
+							"generic",
+							"state",
+							"floral",
+							"scenic",
+							"service",
+							"leather",
+							"missionary",
+							"country",
+							"wreath 4",
+							"leaves",
+							"wreath",
+							"nature",
+							"christmas",
+							"symbolic",
+							"outline",
+							"map",
+							"world"
+						]
+					},
+					"product_image": "https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_mission_generic/Wreath_4.jpg",
+					"product_thumbs": {
+						"thumbnails": [
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Standing_Journal_Photo.jpg",
+							"https://s3-us-west-1.amazonaws.com/parchmentgoods/product-images/journal_thumbnails/Open_Journal_Photo.jpg"
+						]
+					},
+					"product_desc": "Personalized Missionary Journal",
+					"product_type": "journal_everyday",
+					"product_price": 38.99,
+					"product_sale": 29.99,
+					"product_shipping": 3.99,
+					"o1": "[\"name\",\"mission\",\"service dates\",\"scripture\"]"
+				}
+			]
+			// this.props.setProducts(data)
+			console.log("Final Log of Data:",data)
+		})
 	}
 
   routeChange () {
