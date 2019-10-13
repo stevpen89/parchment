ba5c5a63fe223f53c2b3dfa444f7c2ee1f8b3092
Steven Penfield <stevenpenfield9@gmail.com>
2019-10-12 23:39:28 -0600

    added in the new products page, it is responsive as well, giving us flexibility there, also, I believe there was an NPM I globally during this session

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
--- src/components/Nav/ProductCategories/ProductCategories.js
+++ src/components/Nav/ProductCategories/ProductCategories.js
@@ -4,11 +4,31 @@ import './ProductCategories.css';
 import ProdCatTile from './ProdCatTile/ProdCatTile';
 
 const topRowData = [
-	{id:1,title:"Family History Blankets",img:"https://www.sunnyskyz.com/uploads/2018/12/65zli-cat-meme-1a.jpg",desc:"test text",routingString:"/products/blanket/family/history"},
-	{id:2,title:"Family History Prints",img:"https://www.sunnyskyz.com/uploads/2018/12/65zli-cat-meme-1a.jpg",desc:"test text",routingString:"/products/print/family/history"},
-	{id:3,title:"Holiday Blankets",img:"https://www.sunnyskyz.com/uploads/2018/12/65zli-cat-meme-1a.jpg",desc:"test text",routingString:"/products/blanket/holiday"},
-	{id:4,title:"Missionary Journals",img:"https://www.sunnyskyz.com/uploads/2018/12/65zli-cat-meme-1a.jpg",desc:"test text",routingString:"/missionaryJournalSelector"},
-	{id:5,title:"Everyday Journals",img:"https://www.sunnyskyz.com/uploads/2018/12/65zli-cat-meme-1a.jpg",desc:"test text",routingString:"/products/journal/everyday"}]
+	{	id:1,
+		title:"Family History Blankets",
+		img:"https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/carousel/Front_Photo_3.jpg",
+		desc:"Minky Blankets Printed With Your Family Tree",
+		routingString:"/products/blanket/family/history"},
+	{	id:2,
+		title:"Family History Prints",
+		img:"https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/print_banner.jpg",
+		desc:"Photo Quality Prints Of Your Family Tree",
+		routingString:"/products/print/family/history"},
+	{	id:3,
+		title:"Holiday Blankets",
+		img:"https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/holiday_blanket_banner.jpg",
+		desc:"Minky Blankets Printed With A Holiday Pattern - Customizable",
+		routingString:"/products/blanket/holiday"},
+	{	id:4,
+		title:"Missionary Journals",
+		img:"https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/carousel/Flat_Missionary_Journals.jpg",
+		desc:"Customizable, Laser-Engraved Journals For Missionaries",
+		routingString:"/missionaryJournalSelector"},
+	{	id:5,
+		title:"Everyday Journals",
+		img:"https://s3-us-west-1.amazonaws.com/parchmentgoods/home-page/carousel/Standing-Everyday_Journals.jpg",
+		desc:"Customizable, Laser-Engraved Journals For Everyday Use",
+		routingString:"/products/journal/everyday"}]
 
 
 class ProductCategories extends Component {
